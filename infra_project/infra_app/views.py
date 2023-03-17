9dd687ce0e0d5f5647e7159354cd2b3ccbc3664a
aleksandr <aleksospishev@yandex.ru>
2023-03-17 22:41:00 +0300

    add telegram messege

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! telegram работает')
 
 
 def second_page(request):
